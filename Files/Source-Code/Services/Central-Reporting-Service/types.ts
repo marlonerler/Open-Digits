ab7aa2a6fe24e032020483baf8688d0653de7dd4
marlonerler <viridian035@gmail.com>
2023-12-26 12:27:44 +0100

    separate types

---
+++ Files/Source-Code/Services/Central-Reporting-Service/types.ts
@@ -0,0 +1,20 @@
+export enum LogMessageCategories {
+    Activity = "Activity",
+    Warning = "Warning",
+    Error = "Error",
+    Unknown = "unknown",
+}
+
+/** additional information to write alongside message */
+export interface LogMessageData {
+    username?: string;
+    reportingService?: string;
+    category?: LogMessageCategories;
+}
+
+/** object containing all information to write */
+export interface FinalLogMessageObject extends LogMessageData {
+    message: string;
+    timestamp: string;
+    uuid: string;
+}
